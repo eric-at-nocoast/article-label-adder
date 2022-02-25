c375628200bd9e7098c8dc11704bc6a0ac64481c
Eric Nelson <enelson@zendesk.com>
2022-02-25 10:18:38 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Label your Zendesk Help Center articles with the user segment
+
+<!-- Description of project-->
+
+This app loops through your articles and checks to see if they have a user_segment. If it does, then it will add a corresponding label to the article that can be used to filter the articles in the [mobile sdk](https://developer.zendesk.com/documentation/classic-web-widget-sdks/support-sdk/ios/help_center/#filter-articles-by-label)
+​
+
+​
+
+## Getting started
+
+​
+Follow these steps to get a local copy up and running.
+​
+
+<!-- Any required packages or dependencies prior to installation of the app-->
+
+### Prerequisites
+
+​
+
+- npm
+
+```
+npm install npm@latest -g
+```
+
+<!-- Steps to get the app running locally -->
+
+### Installation
+
+1. Clone the repo
+
+```
+git clone https://github.com/eric-at-zd/article-label-adder
+```
+
+2. Install dependancies
+
+```
+npm install
+```
+
+3. Run the app. Unless otherwise specified, this will open on localhost:5000
+
+```
+npm run start
+```
+
+​
---
+++ addLabel.js
@@ -0,0 +1,89 @@
+require("dotenv").config();
+let axios = require("axios");
+
+//Authorization needs to be base64 encoded to be accepted by Zendesk
+const encodedToken = Buffer.from(
+  `${process.env.ZD_USER}/token:${process.env.ZD_TOKEN}`
+).toString("base64");
+
+// This is the config that we use for making our api requests through axios. It will be reassigned with different data for the appropriate call using Object.assign
+let config = {
+  method: "get",
+  url: process.env.ZD_URL + "/api/v2/help_center/user_segments",
+  headers: {
+    Authorization: `Basic ${encodedToken}`,
+    "Content-Type": "application/json",
+  },
+};
+
+//This function receives the current iterations article id. It first checks if the article has a user_segment. If it doesn't it moves on to the next.
+//If it does, then based off that checks the labels attached to the article to see if any of them matches the user_segment.
+//If the user_segment label is not present then it updates the article with it.
+const articleUpdater = async (article, userSegments) => {
+  if (article.user_segment_id != null) {
+    let segmentObj = userSegments.find(
+      ({ id }) => id === article.user_segment_id
+    );
+
+    if (article.label_names.length > 0) {
+      let labelArr = article.label_names;
+
+      let segmentLabel = article.label_names.find(
+        (label) => label == segmentObj.user_type
+      );
+      let articleId = JSON.stringify(article.id);
+      let payload = {
+        article: {
+          label_names: labelArr,
+        },
+      };
+
+      Object.assign(config, {
+        method: "put",
+        url:
+          process.env.ZD_URL +
+          `/api/v2/help_center/en-us/articles/${articleId}`,
+        data: payload,
+      });
+      if (segmentLabel === undefined) {
+        labelArr.push(segmentObj.user_type);
+        const res = await axios(config);
+        const segmentLabel =
+          res.data.article.label_names[res.data.article.label_names.length - 1];
+        return `Article ${res.data.article.id} updated! with the label ${segmentLabel}`;
+      } else if (segmentLabel != undefined) {
+        return `Article ${articleId} already has the user segment label`;
+      }
+    }
+  } else return `Article ${article.id} has no user segment`;
+};
+
+const delay = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
+
+//This function grabs a list of all the articles and then loops through them to the articleUpdater
+const articleGetter = async (userSegments) => {
+  Object.assign(config, {
+    url: process.env.ZD_URL + `/api/v2/help_center/en-us/articles`,
+  });
+  const resp = await axios(config);
+  let resultArr = [];
+  //parsing the ticket id out of the response for use in updating the ticket
+  let articleArr = resp.data.articles;
+  for (i = 0; i < articleArr.length; i++) {
+    await delay();
+    const res = await articleUpdater(articleArr[i], userSegments);
+    resultArr.push(res);
+  }
+  return resultArr;
+};
+
+async function main() {
+  const res = await axios(config);
+  const userSeg = res.data.user_segments;
+  const results = await articleGetter(userSeg);
+  console.log(results);
+}
+
+main().catch((err) => {
+  console.log(err);
+});
